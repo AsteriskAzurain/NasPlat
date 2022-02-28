8474c794f42b8573891699d8564938f58e5f9ee3
Asterisk Azurain <qysx32_kitty@yeah.net>
2022-03-01 01:30:05 +0800

    Init Project

---
+++ nasplat-admin/src/main/java/com/ishang/nasplatadmin/NasplatAdminApplication.java
@@ -0,0 +1,13 @@
+package com.ishang.nasplatadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasplatAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasplatAdminApplication.class, args);
+	}
+
+}
